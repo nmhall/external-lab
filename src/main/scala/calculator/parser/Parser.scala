aa413f78b5641ea0b5bd05070730086c49b59f70
Ben Wiedermann <benw@cs.hmc.edu>
2014-09-30 22:11:40 -0700

    Parsing for addition. BUT, it's right-associative.

--- src/main/scala/calculator/parser/Parser.scala
+++ src/main/scala/calculator/parser/Parser.scala
@@ -3,11 +3,16 @@ package calculator.parser
 import scala.util.parsing.combinator._
 import calculator.ir._
 
-object CalcParser extends JavaTokenParsers {
+object CalcParser extends JavaTokenParsers with PackratParsers {
 
     // parsing interface
-    def apply(s: String): ParseResult[AST] = parseAll(number, s)
+    def apply(s: String): ParseResult[AST] = parseAll(expr, s)
 
+    // expressions
+    lazy val expr: PackratParser[Expr] = 
+      (   expr~"+"~expr ^^ {case l~"+"~r ⇒ Plus(l, r)}
+        | number )
+      
     // numbers
     def number: Parser[Num] = wholeNumber ^^ {s ⇒ Num(s.toInt)}
     
